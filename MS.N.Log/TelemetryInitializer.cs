be409ea52305fd393c077596c9ef9b9b3d36f599
#gONzA~ <facundo.g.miguel@bancogalicia.com.ar>
2018-05-04 16:18:07 -0300

    telemetry log

--- MS.N.Log/TelemetryInitializer.cs
+++ MS.N.Log/TelemetryInitializer.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MS.N.Log
+namespace BGBA.MS.N.Log
 {
     public class TelemetryInitializer : ITelemetryInitializer
     {
