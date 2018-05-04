2f6925619820168f4f330fd490ba6d86e13e6628
Gustavo Fuentes <gustavof@lagash.com>
2018-05-04 15:33:41 -0300

    Refactor: Telemetry

---
+++ MS.N.Log/TelemetryInitializer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MS.N.Log
+{
+    public class TelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            var requestTelemetry = telemetry as RequestTelemetry;
+
+            if (requestTelemetry == null) return;
+
+            telemetry.Context.Cloud.RoleName = "MS.N.Log-ROLE";
+        }
+    }
+}
